a6987243f300a20bf1b52cc7520f5d0a78acb048
Owen Henley <owenhenley@me.com>
2018-08-21 15:54:01 -0600

    Started Storyboards, added Swift files
    Added View Conroller Scenes, New Activity ViewController.swift, temp splash screen

---
+++ ADVNTR/Controllers/ViewControllers/NewActivity/NewActivityViewController.swift
@@ -0,0 +1,31 @@
+//
+//  NewActivityViewController.swift
+//  ADVNTR
+//
+//  Created by Owen Henley on 8/21/18.
+//  Copyright © 2018 Zachary Frew. All rights reserved.
+//
+
+import UIKit
+
+class NewActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
