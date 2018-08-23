398c86e1499c5779537daf9db0f12156714687ef
Owen Henley <owenhenley@me.com>
2018-08-23 16:26:34 -0600

    Custom Activity Update, Seques

---
+++ ADVNTR/Controllers/ViewControllers/LaunchScreen/ForgotPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ForgotPasswordViewController.swift
+//  ADVNTR
+//
+//  Created by Owen Henley on 8/23/18.
+//  Copyright © 2018 Zachary Frew. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
