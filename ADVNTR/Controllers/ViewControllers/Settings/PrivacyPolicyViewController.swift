09960a8abbbddc0f9910d4cb95fadc1d614b19c4
Owen Henley <owenhenley@me.com>
2018-08-22 18:05:37 -0600

    Added some Setting VCs

--- ADVNTR/Controllers/ViewControllers/Settings/PrivacyPolicyViewController.swift
+++ ADVNTR/Controllers/ViewControllers/Settings/PrivacyPolicyViewController.swift
@@ -1,24 +1,19 @@
 //
-//  SettingsViewController.swift
+//  PrivacyPolicyViewController.swift
 //  ADVNTR
 //
-//  Created by Zachary Frew on 8/20/18.
+//  Created by Owen Henley on 8/22/18.
 //  Copyright © 2018 Zachary Frew. All rights reserved.
 //
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class PrivacyPolicyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
@@ -27,7 +22,7 @@ class SettingsViewController: UIViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
+        // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
     */
