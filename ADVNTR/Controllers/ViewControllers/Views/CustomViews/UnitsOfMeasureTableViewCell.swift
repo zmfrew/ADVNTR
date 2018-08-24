eeca4c8e1e25335c9364fa7ed50b981f9cda66b1
Owen Henley <owenhenley@me.com>
2018-08-24 16:36:30 -0600

    Views Updates

---
+++ ADVNTR/Controllers/ViewControllers/Views/CustomViews/UnitsOfMeasureTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UnitsOfMeasureTableViewCell.swift
+//  ADVNTR
+//
+//  Created by Owen Henley on 8/24/18.
+//  Copyright © 2018 ADVNTR. All rights reserved.
+//
+
+import UIKit
+
+class UnitsOfMeasureTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
