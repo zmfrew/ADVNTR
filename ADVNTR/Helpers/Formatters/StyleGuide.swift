bcf3a34c933adef95b44a56d0241136789ee2b1c
Peter Gow <petergow@Peters-MacBook-Pro.local>
2018-08-28 03:12:54 +1000

    Firebase and SwiftEntryKit dependencies installed using Firebase. Adds User model object and Activity model object, as well as controllers for both.

---
+++ ADVNTR/Helpers/Formatters/StyleGuide.swift
@@ -0,0 +1,25 @@
+//
+//  StyleGuide.swift
+//  ADVNTR
+//
+//  Created by Peter Gow on 28/8/18.
+//  Copyright © 2018 ADVNTR. All rights reserved.
+//
+
+import UIKit
+import SwiftEntryKit
+extension UIFont {
+    
+    //Fonts
+    static let alertTitle = UIFont.init(name: "Avenir", size: 18)
+    static let alertDescription = UIFont.init(name: "Avenir", size: 14)
+}
+
+extension UIColor {
+    
+    //Colours
+    static let alertRed = UIColor(red: 216/255, green: 66/255, blue: 70/255, alpha: 1)
+    static let alertGreen = UIColor(red: 31/255, green: 130/255, blue: 117/255, alpha: 1)
+    static let darkWhite = UIColor(white: 218/255, alpha: 1)
+    static let lightWhite = UIColor(white: 250/255, alpha: 1)
+}
--- ADVNTR/Helpers/Managers/NetworkManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NetworkManager.swift
-//  ADVNTR
-//
-//  Created by Zachary Frew on 8/20/18.
-//  Copyright © 2018 Zachary Frew. All rights reserved.
-//
-
-import Foundation
