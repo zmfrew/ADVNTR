36fd5916e9d7f969a17205fb2d1623657fa4117d
Owen Henley <owenhenley@me.com>
2018-09-18 15:32:07 -0600

    cell fix

---
+++ ADVNTR/Controllers/ViewControllers/Views/CustomViews/CurrentVersionTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CurrentVersionTableViewCell.swift
+//  ADVNTR
+//
+//  Created by Owen Henley on 9/18/18.
+//  Copyright © 2018 ADVNTR. All rights reserved.
+//
+
+import UIKit
+
+class CurrentVersionTableViewCell: UITableViewCell {
+
+
+}
